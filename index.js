6c28c80fde9b285f1209195531dfb941c9baf202
samwaseee <samwaseee@gmail.com>
2024-06-05 01:44:49 +0600

    indivual camps get query updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -26,13 +26,33 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    //await client.connect();
+
+    const campCollection = client.db('careCamp').collection('camp');
+
+    app.get('/camps', async (req, res) => {
+      const result = await campCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get('/camps/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await campCollection.findOne(query);
+      res.send(result);
+    });
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    //await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
@@ -40,9 +60,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Camp is running')
+  res.send('Camp is running')
 })
 
 app.listen(port, () => {
-    console.log(`Camp is running om ${port}`);
+  console.log(`Camp is running om ${port}`);
 })
